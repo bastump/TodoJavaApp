b3ab6fab3771a4edaa071db253a8da2b45d51599
Brian Stump <brianstump@Brians-MacBook-Pro-2017.local>
2019-10-10 20:54:15 -0400

    formatted pom.xml, fixed imports, added manifest.

--- src/test/java/com/bastump/todoapi/TodoControllerTests.java
+++ src/test/java/com/bastump/todoapi/TodoControllerTests.java
@@ -1,8 +1,8 @@
 package com.bastump.todoapi;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
 
